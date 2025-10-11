e9037942e96ee59c95b6667fcb8f7a9df626c6bb
Rohit8681 <rohitpanchal2309@gmail.com>
2025-10-11 19:07:11 +0530

    Fix | Tripclose command create and cron create

---
+++ app/Console/Kernel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->command('trip:close')->dailyAt('23:58'); // Har din midnight 12:00 AM par
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+        require base_path('routes/console.php');
+    }
+}
