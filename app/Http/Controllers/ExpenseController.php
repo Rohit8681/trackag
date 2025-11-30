5258ae35d70d3f5d44716d10abd78ca05c3c17d8
Rohit8681 <rohitpanchal2309@gmail.com>
2025-11-30 10:00:18 +0530

    f

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -225,6 +225,7 @@ class ExpenseController extends Controller
 
     public function bulkApprove(Request $request)
     {
+        dd($request->all());
         $ids = json_decode($request->trip_ids, true);
 
         if (empty($ids)) {
