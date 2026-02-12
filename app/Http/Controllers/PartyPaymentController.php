dba7462f7b19d41ae041a65dcb69de9c6c905bca
Rohit8681 <rohitpanchal2309@gmail.com>
2026-02-12 22:22:10 +0530

    Fix | New changes

--- app/Http/Controllers/PartyPaymentController.php
+++ app/Http/Controllers/PartyPaymentController.php
@@ -63,11 +63,11 @@ class PartyPaymentController extends Controller
         }
 
         if (in_array($roleName, ['master_admin', 'sub_admin'])) {
-            $employees = User::where('status', 'Active')->get();
+            $employees = User::where('status', 'Active')->where('id', '!=', 1)->get();
         } else {
             $employees = empty($stateIds)
                 ? collect()
-                : User::where('status', 'Active')
+                : User::where('status', 'Active')->where('id', '!=', 1)
                     ->whereIn('state_id', $stateIds)
                     ->where('reporting_to', $user->id)
                     ->get();
