c946b15d336f48781510f88abbcced1fb26c08da
Rohit8681 <rohitpanchal2309@gmail.com>
2026-02-16 23:13:33 +0530

    FIx | New changes

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -494,6 +494,7 @@ class ExpenseController extends Controller
 
     public function bulkApprove(Request $request)
     {
+        dd($request->all());
         $ids = json_decode($request->trip_ids, true);
         $selected_user_id = $request->selected_user_id;
 
