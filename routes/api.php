1b4614284c16b1f96b9f643bb03346a013fcc320
Rohit8681 <rohitpanchal2309@gmail.com>
2026-02-26 21:55:13 +0530

    Fix | New changes

--- routes/api.php
+++ routes/api.php
@@ -74,6 +74,8 @@ Route::middleware([TenantAuthenticate::class])->group(function () {
 
     Route::get('product-list', [OrderController::class, 'getProductList']);
     Route::get('product-packings', [OrderController::class, 'getProductPackings']);
+    Route::get('packing-details', [OrderController::class, 'getPackingDetails']);
+    
     Route::post('orders-store', [OrderController::class, 'store']);
     Route::get('orders-list', [OrderController::class, 'index']);
     
