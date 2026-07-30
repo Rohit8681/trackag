82f6b4bc0effa68d277ac92bfb8ecdba24a7831b
Rohit8681 <rohitpanchal2309@gmail.com>
2026-07-31 00:06:38 +0530

    Fix | New chnages

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,10 +15,10 @@ class Kernel extends ConsoleKernel
         $schedule->command('trip:close')->everyMinute();
 
         // Original live schedule:
-        // $schedule->command('trip:close-reminder')->hourly()->between('21:00', '23:59')->timezone('Asia/Kolkata');
-        // $schedule->command('party-visit:checkout-reminder')->hourly()->timezone('Asia/Kolkata');
-        $schedule->command('trip:close-reminder')->everyMinute()->timezone('Asia/Kolkata');
-        $schedule->command('party-visit:checkout-reminder')->everyMinute()->timezone('Asia/Kolkata');
+        $schedule->command('trip:close-reminder')->hourly()->between('21:00', '23:59')->timezone('Asia/Kolkata');
+        $schedule->command('party-visit:checkout-reminder')->hourly()->timezone('Asia/Kolkata');
+        // $schedule->command('trip:close-reminder')->everyMinute()->timezone('Asia/Kolkata');
+        // $schedule->command('party-visit:checkout-reminder')->everyMinute()->timezone('Asia/Kolkata');
     }
 
     /**
